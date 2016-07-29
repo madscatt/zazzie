d5b2322a19d723e2a64973ef36755ccc2ae18a9b
ianhi <ianhuntisaak@gmail.com>
2016-07-29 14:06:38 -0400

    Fix integrator
    make works_integrate acutally work....

--- src/sassie/calculate/sascalc_pbc/calc_gr/works_integrate_gr.py
+++ src/sassie/calculate/sascalc_pbc/calc_gr/works_integrate_gr.py
@@ -1,68 +1,59 @@
-import string,locale
+import string
+import locale
 from scipy.integrate import simps
-from scipy.integrate import trapz
 import numpy
+import matplotlib.pyplot as plt
 
 
-def integrand(i,q,r,gr):
+def integrate_gr(r, gr, rho=.02, nQ=500, loQ=0, hiQ=15, cut=-1):
+    from scipy.integrate import simps
 
-	value = (gr[i] - 1.0) * r[i] * numpy.sin(q * r[i])
-	
-	return value
+    r = numpy.asanyarray(r)
+    gr = numpy.asanyarray(gr)
+
+    def integrand(q, r, gr):
+        return (gr - 1.0) * r * numpy.sin(q * r)
+
+    r = r[:cut]
+    gr = gr[:cut]
+    Q = numpy.linspace(loQ, hiQ, nQ)
+    fact = 4 * numpy.pi * rho
+
+    sq = numpy.zeros_like(Q)
+    for i, q in enumerate(Q):
+        sq[i] = 1 + fact / q * simps(integrand(q, r, gr), r)
+    return (Q, sq)
+
 
 def read_gr(filename):
 
-	data=open(filename,'r').readlines()
-	nl=len(data)
-	r=numpy.zeros(nl,numpy.float)
-	gr=numpy.zeros(nl,numpy.float)
+    data = open(filename, 'r').readlines()
+    nl = len(data)
+    r = numpy.zeros(nl, numpy.float)
+    gr = numpy.zeros(nl, numpy.float)
 
-	for i in xrange(nl):
-		lin=string.split(data[i])
-		tr=locale.atof(lin[0])
-		tgr=locale.atof(lin[1])
-		r[i]=tr ; gr[i]=tgr
+    for i in xrange(nl):
+        lin = string.split(data[i])
+        tr = locale.atof(lin[0])
+        tgr = locale.atof(lin[1])
+        r[i] = tr
+        gr[i] = tgr
 
-	return r,gr
+    return r, gr
 
-if __name__=='__main__': 
+if __name__ == '__main__':
 
-	pi = numpy.pi
+    filename = 'gr_argon.dat'
 
-	filename = 'argon_gr.txt'
+    r, gr = numpy.loadtxt(filename, unpack=True, usecols=[0, 2])
+    q, sq = integrate_gr(r, gr)
 
-	r,gr = read_gr(filename)
+    # Save output
+    numpy.savez('output', q=q, sq=sq)
 
-	highq = 12.0
-	nsteps = len(r)
-	stepq = (highq)/nsteps
-
-	stepq = 0.0294
-	nsteps = 399
-
-	lowq = stepq
-	
-	outfile=open('gr.int','w')
-
-#	no = 1.0/864.0
-#	fact = 4 * pi * no
-
-	correction = 2.7013/2.986804
-	print 'correction = ',correction
-
-	no = 0.02125
-	fact = no * correction
-
-	for s in xrange(nsteps+1):
-		this_q = lowq + stepq*s
-		value = numpy.zeros(len(r),numpy.float)
-		for i in xrange(len(r)):
-			value[i] = integrand(i,this_q,r,gr)
-
-		sq = (fact*simps(value)/this_q) + 1
-		sq2 = (fact*trapz(value)/this_q) + 1
-
-		outfile.write('%f\t%f\t%f\n' % (this_q,sq,sq2))
-
-	outfile.close()
-
+    # plot output
+    plt.style.use('ggplot')
+    plt.xlabel('Q')
+    plt.ylabel('s(Q)')
+    plt.plot(q, sq)
+    plt.show()
