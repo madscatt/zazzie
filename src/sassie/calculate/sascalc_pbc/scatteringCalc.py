413dba12d8b084d06941d9fa0d52127ee2416f00
ianhi <ianhuntisaak@gmail.com>
2016-07-29 15:11:01 -0400

    pure numpy calculator

--- src/sassie/calculate/sascalc_pbc/scatteringCalc.py
+++ src/sassie/calculate/sascalc_pbc/scatteringCalc.py
@@ -2,6 +2,7 @@ from __future__ import division
 import matplotlib.pyplot as plt
 import numpy as np
 import sasmol.sasmol as sasmol
+import numexpr as ne
 from compiledUtils._cLoops import ffi, lib
 from lennard_gofr import *
 import matplotlib as mpl
@@ -14,16 +15,16 @@ from multiprocessing import Pool, cpu_count
 print(datetime.datetime.now())
 startTime = datetime.datetime.now()
 
-pdb_NAME = 'Data/small/final-arg-5.pdb'
-dcd_NAME = 'Data/small/run_1.dcd'
+pdb_NAME = '/home/data/sascalc_pbc/pdbs/run_0.pdb'
+dcd_NAME = '/home/data/sascalc_pbc/pdbs/run_1.dcd'
 
-startFrame = 2
+startFrame =995 
 endFrame   =  -1 #-1 = use all
-NUM_Q      = 250
+NUM_Q      = 100
 START_Q    = -1
 END_Q      = 1.6
 NUM_GV       = 27
-
+sigma=3.4
 
 mol = sasmol.SasMol(0)
 mol.read_pdb(pdb_NAME)
@@ -60,22 +61,40 @@ def process_frame(frame):
             I_tmp += lib.sQ(cast_coor,cast_q,num,num)
         I[i] = I_tmp/len(gv)
     return I
+def process_frame_new(frame):
+    """
+    modified to be purely numpy/numexpr
+    """
+    I = np.zeros(len(Q_list))
+    disp = np.zeros((len(coor[frame]),len(coor[frame]),3))
+    for i in xrange(len(coor[frame])):
+        for j in xrange(len(coor[frame])):
+            disp[i][j] = coor[frame][i]-coor[frame][j]
+    reshaped = disp.reshape((mol.natoms()**2,3))*sigma # sigma from reduced units
+    dotted = np.inner(gv,reshaped)
+    for i,Q in enumerate(Q_list):
+        inner = dotted*Q
+        I[i] = ne.evaluate("sum(cos(inner))")
+    I /= len(gv)
+    return I
 pool = Pool(processes=cpu_count())              # process per core
-I_mp=pool.map(process_frame, frames)
+#I_mp=pool.map(process_frame, frames)
+I_mp=pool.map(process_frame_new, frames)
 
-def getOutputDir():
+def getOutputDir(base=''):
     '''
     Returns a path to a time stamped directory under the Outputs folder as a string
     '''
     import os
     from time import strftime
-    directory = "Outputs/"+strftime("%Y-%m-%d_%H-%M")
+    directory = base+"Outputs/"+strftime("%Y-%m-%d_%H-%M")
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory+"/"
 
 endTime = datetime.datetime.now()
-outdir = getOutputDir()
+outdir = getOutputDir('/home/data/')
+print('output: '+outdir)
 f = open(outdir+'EXP_info.txt','w')
 f.write('Exp Method')
 f.write('\n\nSTART_Q,\t'+str(START_Q))
@@ -93,5 +112,9 @@ f.write('\nendTime,\t'+str(endTime))
 f.close()
 pickle.dump( I_mp, open( outdir+"multiFrame-"+str(frames[0])+'-'+str(frames[-1])+'_'+str(START_Q)+
 		'-'+str(END_Q)+'_'+str(NUM_Q), "wb" ) )
-np.save(outdir + 'outPutI-Q'+str(NUM_Q),I_mp)
+np.save(outdir + 'outPutI-Q' + str(NUM_Q), I_mp)
+np.save(outdir + 'Q_list', Q_list)
+
+np.save('outPutI-Q' + str(NUM_Q), I_mp)
+np.save('Q_list', Q_list)
 print(datetime.datetime.now())
