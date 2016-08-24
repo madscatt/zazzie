28bea5ab13e3f315951b563d45981b0fbf03da61
Steven C. Howell <steven.howell@nist.gov>
2016-08-24 16:10:48 -0400

    add header documentation to file

--- src/scripts/convergence_test.py
+++ src/scripts/convergence_test.py
@@ -1,4 +1,23 @@
-# $Id: convergence_test.py 3188 2016-06-13 16:58:12Z schowell $
+# from __future__ import absolute_import
+# from __future__ import division
+# from __future__ import print_function
+# # from __future__ import unicode_literals
+
+"""SASSIE: Copyright (C) 2011-2015 Joseph E. Curtis, Ph.D.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import glob
 import logging
 import numpy
@@ -7,14 +26,21 @@ import pandas
 import time
 import sasmol.sasmol as sasmol
 
+# allows for creating plots without an xserver
 try:
     dummy = os.environ["DISPLAY"]
 except:
-    # allows for creating plots without an xserver
     import matplotlib
     matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+
+#       convergence_test
+#
+#       08/24/2016      --      updating for github repo        :       sch
+#
+#        1         2         3         4         5         6         7
+# 34567890123456789012345678901234567890123456789012345678901234567890123456789
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
