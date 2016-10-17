c56da2db905b9cf693fe279fe18038a547501e01
Dave Wright <dave.william.wright@gmail.com>
2016-10-17 10:20:43 +0100

    Code tidying

--- src/sassie/build/pdbrx/pdbrx/apply_psfgen.py
+++ src/sassie/build/pdbrx/pdbrx/apply_psfgen.py
@@ -12,7 +12,7 @@
 	pdb file and associated psf file.  Hydrogens and some missing atoms are
 	added as well.  Atom names are changed to comply with CHARMM naming
 	conventions.
-	
+
 	This program will process all files in a specified directory sequentially.
 
 	Input values are listed at the bottom of this file.
@@ -30,13 +30,13 @@
 
 '''
 
+import logging
+import os
+import subprocess
+
 import sasmol.sasmol as sasmol
 from sassie.util import sasconfig
 import sassie.build.pdbscan.pdbscan.pdbscan_utils as utils
-
-import logging
-import os, sys, string, glob, numpy
-import subprocess
 
 
 class PsfgenDriver():
@@ -100,15 +100,17 @@ class PsfgenDriver():
 
         script_file = open(script_filename)
 
-        log_file = open(log_filename,'w')
+        log_file = open(log_filename, 'w')
 
         # Run PSFGEN
-        failure = subprocess.call([self.psfgen_exe], stdin=script_file, stdout=log_file, stderr=subprocess.STDOUT)
+        failure = subprocess.call(
+            [self.psfgen_exe], stdin=script_file, stdout=log_file, stderr=subprocess.STDOUT)
 
         log_file.close()
 
         if failure:
-            self.logger.critical('ERROR: Unsuccessful PSFGEN run, check log for details')
+            self.logger.critical(
+                'ERROR: Unsuccessful PSFGEN run, check log for details')
 
         return
 
@@ -122,11 +124,11 @@ class PsfgenDriver():
         out_path = self.out_path
         out_prefix = self.out_prefix
 
-        st = self.script_lines
+        out_lines = self.script_lines
 
-        st.append('')
-        st.append('topology {0:s}'.format(self.top_file_path))
-        st.append('')
+        out_lines.append('')
+        out_lines.append('topology {0:s}'.format(self.top_file_path))
+        out_lines.append('')
 
         self.add_standard_aliases_to_script()
         self.add_nucleic_aliases_to_script()
@@ -136,19 +138,18 @@ class PsfgenDriver():
 
         self.add_disulphides_script()
 
-        st.append('')
-        st.append('guesscoord')
-        st.append('')
+        out_lines.append('')
+        out_lines.append('guesscoord')
+        out_lines.append('')
 
-        out_location_stub = os.path.join(out_path,out_prefix)
+        out_location_stub = os.path.join(out_path, out_prefix)
 
-        st.append('writepsf {0:s}_xplor.psf'.format(out_location_stub))
-        st.append('writepdb {0:s}.pdb'.format(out_location_stub))
+        out_lines.append('writepsf {0:s}_xplor.psf'.format(out_location_stub))
+        out_lines.append('writepdb {0:s}.pdb'.format(out_location_stub))
 
+        out_lines.append('writepsf charmm {0:s}.psf'.format(out_location_stub))
 
-        st.append('writepsf charmm {0:s}.psf'.format(out_location_stub))
-
-        st.append('')
+        out_lines.append('')
 
         return
 
@@ -165,7 +166,7 @@ class PsfgenDriver():
         mol = self.mol
         info = self.segname_info
         logger = self.logger
-        st = self.script_lines
+        out_lines = self.script_lines
 
         # Create segment PDB file for PSFGEN run
         out_filename = '{0:s}.pdb'.format(segname)
@@ -190,37 +191,38 @@ class PsfgenDriver():
         nterm_resname = seg_mol.resname()[0]
 
         # Create psfgen input section for segname
-        st.append('segment {0:s} {{'.format(segname))
+        out_lines.append('segment {0:s} {{'.format(segname))
 
-        st.append('\tpdb {0:s}'.format(output_pdb))
+        out_lines.append('\tpdb {0:s}'.format(output_pdb))
 
         if moltype == 'protein':
             self.add_nterm_patch(nterm_resname, True)
-            st.append('\tlast CTER')
-        if moltype in ['nucleic','rna','dna']:
-            st.append('\tfirst 5TER')
-            st.append('\tlast 3TER')
+            out_lines.append('\tlast CTER')
+        if moltype in ['nucleic', 'rna', 'dna']:
+            out_lines.append('\tfirst 5TER')
+            out_lines.append('\tlast 3TER')
 
-        st.append('}')
-        st.append('')
+        out_lines.append('}')
+        out_lines.append('')
 
-        st.append('coordpdb {0:s} {1:s}'.format(output_pdb, segname))
+        out_lines.append('coordpdb {0:s} {1:s}'.format(output_pdb, segname))
 
         if moltype == 'dna':
 
-            st.append('')
+            out_lines.append('')
 
-            res_desc = utils.uniquify_list(zip(seg_mol.resid(), seg_mol.resname()))
+            res_desc = utils.uniquify_list(
+                zip(seg_mol.resid(), seg_mol.resname()))
             for resid, resname in res_desc:
                 self.apply_dna_patch(segname, resid, resname)
 
-            st.append('')
+            out_lines.append('')
 
         return
 
     def add_disulphides_script(self):
 
-        st = self.script_lines
+        out_lines = self.script_lines
 
         chosen = self.mol.segnames()
 
@@ -233,100 +235,100 @@ class PsfgenDriver():
             residue2 = bound[1]
 
             if (residue1['subdiv'] in chosen) and (residue2['subdiv'] in chosen):
-                st.append('patch DISU {0:s}:{1:d} {2:s}:{3:d}'.format(residue1['subdiv'], residue1['resid'],
-                                                                      residue2['subdiv'], residue2['resid']))
+                out_lines.append('patch DISU {0:s}:{1:d} {2:s}:{3:d}'.format(residue1['subdiv'], residue1['resid'],
+                                                                             residue2['subdiv'], residue2['resid']))
 
         return
 
     def add_standard_aliases_to_script(self):
 
-        st = self.script_lines
+        out_lines = self.script_lines
 
-        st.append('')
-        st.append('alias residue HIS HSE')
-        st.append('alias atom ILE CD1 CD')
-        st.append('alias atom SER HG HG1')
-        st.append('alias atom CYS HG HG1')
-        st.append('alias residue HOH TIP3')
+        out_lines.append('')
+        out_lines.append('alias residue HIS HSE')
+        out_lines.append('alias atom ILE CD1 CD')
+        out_lines.append('alias atom SER HG HG1')
+        out_lines.append('alias atom CYS HG HG1')
+        out_lines.append('alias residue HOH TIP3')
 
-        st.append('alias residue UNK GLY')
-        st.append('')
+        out_lines.append('alias residue UNK GLY')
+        out_lines.append('')
 
         return
 
     def add_nucleic_aliases_to_script(self):
 
-        st = self.script_lines
+        out_lines = self.script_lines
 
-        st.append('')
+        out_lines.append('')
 
-        st.append('alias residue G GUA')
-        st.append('alias residue C CYT')
-        st.append('alias residue A ADE')
-        st.append('alias residue T THY')
-        st.append('alias residue U URA')
+        out_lines.append('alias residue G GUA')
+        out_lines.append('alias residue C CYT')
+        out_lines.append('alias residue A ADE')
+        out_lines.append('alias residue T THY')
+        out_lines.append('alias residue U URA')
 
-        st.append('alias residue DG GUA')
-        st.append('alias residue DC CYT')
-        st.append('alias residue DA ADE')
-        st.append('alias residue DT THY')
+        out_lines.append('alias residue DG GUA')
+        out_lines.append('alias residue DC CYT')
+        out_lines.append('alias residue DA ADE')
+        out_lines.append('alias residue DT THY')
 
-        for base in ['GUA','CYT', 'ADE', 'THY', 'URA']:
-            st.append("alias atom {0:s} O5* O5'".format(base))
-            st.append("alias atom {0:s} C5* C5'".format(base))
-            st.append("alias atom {0:s} O4* O4'".format(base))
-            st.append("alias atom {0:s} C4* C4'".format(base))
-            st.append("alias atom {0:s} C3* C3'".format(base))
-            st.append("alias atom {0:s} O3* O3'".format(base))
-            st.append("alias atom {0:s} C2* C2'".format(base))
-            st.append("alias atom {0:s} O2* O2'".format(base))
-            st.append("alias atom {0:s} C1* C1'".format(base))
-            st.append("alias atom {0:s} O2* O2'".format(base))
-            st.append("alias atom {0:s} OP1 O1P".format(base))
-            st.append("alias atom {0:s} OP2 O2P".format(base))
+        for base in ['GUA', 'CYT', 'ADE', 'THY', 'URA']:
+            out_lines.append("alias atom {0:s} O5* O5'".format(base))
+            out_lines.append("alias atom {0:s} C5* C5'".format(base))
+            out_lines.append("alias atom {0:s} O4* O4'".format(base))
+            out_lines.append("alias atom {0:s} C4* C4'".format(base))
+            out_lines.append("alias atom {0:s} C3* C3'".format(base))
+            out_lines.append("alias atom {0:s} O3* O3'".format(base))
+            out_lines.append("alias atom {0:s} C2* C2'".format(base))
+            out_lines.append("alias atom {0:s} O2* O2'".format(base))
+            out_lines.append("alias atom {0:s} C1* C1'".format(base))
+            out_lines.append("alias atom {0:s} O2* O2'".format(base))
+            out_lines.append("alias atom {0:s} OP1 O1P".format(base))
+            out_lines.append("alias atom {0:s} OP2 O2P".format(base))
 
-        st.append('alias atom THY C7 C5M')
-        st.append('alias atom THY C5A C5M')
+        out_lines.append('alias atom THY C7 C5M')
+        out_lines.append('alias atom THY C5A C5M')
 
-        st.append('')
+        out_lines.append('')
 
     def add_nterm_patch(self, nterm_resname, flag):
 
-        st = self.script_lines
+        out_lines = self.script_lines
 
         if not flag:
 
-            if (nterm_resname == 'GLY' or nterm_resname == 'PRO'):
+            if nterm_resname == 'GLY' or nterm_resname == 'PRO':
 
-                st.append('\tfirst NONE')
+                out_lines.append('\tfirst NONE')
 
             else:
 
-                st.append('\tfirst NTER')
+                out_lines.append('\tfirst NTER')
 
         elif flag:
 
             if nterm_resname == 'GLY':
 
-                st.append('\tfirst GLYP')
+                out_lines.append('\tfirst GLYP')
 
             elif nterm_resname == 'PRO':
 
-                st.append('\tfirst PROP')
+                out_lines.append('\tfirst PROP')
 
             else:
 
-                st.append('\tfirst NTER')
+                out_lines.append('\tfirst NTER')
 
         return
 
     def apply_dna_patch(self, segname, resid, resname):
 
-        st = self.script_lines
+        out_lines = self.script_lines
 
-        if resname in ['NUSA','NUSG','DA','DG','ADE','GUA']:
-            st.append('patch DEO2 {0:s}:{1:d}'.format(segname,resid))
+        if resname in ['NUSA', 'NUSG', 'DA', 'DG', 'ADE', 'GUA']:
+            out_lines.append('patch DEO2 {0:s}:{1:d}'.format(segname, resid))
         else:
-            st.append('patch DEO1 {0:s}:{1:d}'.format(segname,resid))
+            out_lines.append('patch DEO1 {0:s}:{1:d}'.format(segname, resid))
 
         return
