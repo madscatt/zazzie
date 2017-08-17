725ce146c9c10b9aeeb8de66da34b2c50280e99a
jc <jc@P641697.campus.nist.gov>
2017-08-17 18:00:54 -0400

    added multi_run folder with code to run pdbrx with many cores at the same time ; draft

---
+++ src/sassie/build/pdbrx/multi_run/multicore_run.py
@@ -0,0 +1,113 @@
+import os, glob
+import multiprocessing 
+
+import traceback
+import time
+
+class Process(multiprocessing.Process):
+    def __init__(self, *args, **kwargs):
+        multiprocessing.Process.__init__(self, *args, **kwargs)
+        self._pconn, self._cconn = multiprocessing.Pipe()
+        self._exception = None
+
+    def run(self):
+        try:
+            multiprocessing.Process.run(self)
+            self._cconn.send(None)
+        except Exception as e:
+            tb = traceback.format_exc()
+            self._cconn.send((e, tb))
+            # raise e  # You can still rise this exception if you need to
+
+    @property
+    def exception(self):
+        if self._pconn.poll():
+            self._exception = self._pconn.recv()
+        return self._exception
+
+
+def run_pdbrx(start, end, all_pdb_files, completed_pdb_files_directory, failed_pdb_files_directory):
+
+	#time.sleep(10)
+	fail = False
+
+	for job in xrange(end - start + 1):
+		this_file = all_pdb_files[job + start]
+		head, tail = os.path.split(this_file)	
+		print 'cp ' + this_file + ' ' + completed_pdb_files_directory + os.sep + tail + '_' + str(start)
+		os.system('cp ' + this_file + ' ' + completed_pdb_files_directory + os.sep + tail + '_' + str(start))
+
+#		if job == 1:
+#			fail = True	
+		if fail:
+			raise ValueError('Something went wrong...')
+
+	return 
+
+
+def main(all_pdb_files_directory, completed_pdb_files_directory, failed_pdb_files_directory, number_of_cores):
+	
+	all_pdb_files = glob.glob(all_pdb_files_directory + os.sep + '*.pdb')
+
+	number_of_files = len(all_pdb_files)
+
+	print 'number of pdb files found = ', number_of_files
+
+	if(not os.path.exists(completed_pdb_files_directory)):
+		os.system('mkdir ' + completed_pdb_files_directory)
+
+	if(not os.path.exists(failed_pdb_files_directory)):
+		os.system('mkdir ' + failed_pdb_files_directory)
+
+	start = [] ; end = []
+
+	remainder = number_of_files % number_of_cores
+	divisor = number_of_files / number_of_cores
+	print 'divisor = ', divisor
+	print 'remainder = ', remainder
+
+	'''
+number of pdb files found =  7
+divisor =  2
+remainder =  1
+start =  [0, 3, 6]
+end =  [2, 5, 6]
+	'''
+
+	for i in xrange(number_of_cores):
+		if i < number_of_cores - 1:
+			this_start = i * (divisor + 1)
+			start.append(this_start)
+			end.append(this_start + divisor)
+		else:
+			this_start = i * (divisor + 1)
+			start.append(this_start)
+			end.append(this_start + remainder - 1)
+
+	print 'start = ', start
+	print 'end = ', end
+
+	all_jobs = []
+        q = multiprocessing.Queue()
+
+	for i in xrange(number_of_cores):
+  
+     		try: 
+			this_process = multiprocessing.Process(target=run_pdbrx, args=(start[i], end[i], all_pdb_files, completed_pdb_files_directory, failed_pdb_files_directory))
+			this_process.start()
+		except Exception as error:
+			print 'error = ', error
+   
+		all_jobs.append(this_process)
+
+	return
+
+if __name__ == '__main__':
+
+	number_of_cores = 3
+
+	all_pdb_files_directory = 'pdb_files'
+	completed_pdb_files_directory = 'completed_pdb_files'
+	failed_pdb_files_directory = 'failed_pdb_files'
+
+	main(all_pdb_files_directory, completed_pdb_files_directory, failed_pdb_files_directory, number_of_cores)
---
+++ src/sassie/build/pdbrx/multi_run/original_gui_mimic_align.py
@@ -0,0 +1,185 @@
+'''
+Driver method to run the align module
+'''
+
+import sys
+import os
+import shutil
+import time
+import sassie.tools.align as align
+#import align as align
+import sassie.interface.input_filter as input_filter
+import sassie.interface.align_filter as align_filter
+import multiprocessing
+
+def user_variables(self, **kwargs):
+
+    ### BEGIN USER INPUT ###
+    ### BEGIN USER INPUT ###
+    ### BEGIN USER INPUT ###
+
+    self.runname = 'run_0'
+    self.inpath = './'
+    self.pdbmol1 = os.path.join(self.inpath, 'hiv1_gag.pdb')
+    self.pdbmol2 = os.path.join(self.inpath, 'hiv1_gag.pdb')
+#    self.infile = os.path.join(self.inpath, 'hiv1_gag.pdb')
+    self.infile = os.path.join(self.inpath, 'hiv1_gag_20_frames.dcd')
+#    self.ofile = 'aligned_hiv1_gag.pdb'
+    self.ofile = 'aligned_hiv1_gag_20_frames.dcd'
+    self.basis1 = 'CA'
+    self.basis2 = 'CA'
+    self.lowres1 = '145'
+    self.lowres2 = '145'
+    self.highres1 = '350'
+    self.highres2 = '350'
+    self.ebasis1 = 'None'
+    self.ebasis2 = 'None'
+    self.ebasis2 = 'None'
+    self.zflag = 'False'
+#    self.zflag = 'True'
+    self.zcutoff = '-66.0' #If there are ANY atoms with a z-value less than the cutoff the frame will not be written to disk.
+    self.testflag = False
+
+    ### END USER INPUT ###
+    ### END USER INPUT ###
+    ### END USER INPUT ###
+
+
+def test_variables(self, paths):
+    '''
+    users of gui_mimic as a driver script to run this module should not edit the values below as they
+    are used for development tests
+
+    this module defines variables that will be used to test the module as well as its input filter
+    variables are defined outside the gui_mimic_align class so that they can be used by these other programs
+
+    '''
+
+    pdb_data_path = paths['pdb_data_path']
+    dcd_data_path = paths['dcd_data_path']
+    module_data_path = paths['module_data_path']
+    other_data_path = paths['other_data_path']
+
+    self.runname = 'run_0'
+    self.inpath = ''
+    self.pdbmol1 = os.path.join(pdb_data_path, 'hiv1_gag.pdb')
+    self.pdbmol2 = os.path.join(pdb_data_path, 'hiv1_gag.pdb')
+    self.infile = os.path.join(dcd_data_path, 'hiv1_gag_20_frames.dcd')
+    self.ofile = 'aligned_hiv1_gag_20_frames.dcd'
+    self.basis1 = 'CA'
+    self.basis2 = 'CA'
+    self.lowres1 = '145'
+    self.lowres2 = '145'
+    self.highres1 = '350'
+    self.highres2 = '350'
+    self.ebasis1 = 'None'
+    self.ebasis2 = 'None'
+
+    self.zflag = 'False'
+    self.zcutoff = '0.0'
+
+    self.testflag = True
+    self.precision = 3
+
+
+def run_module(self, **kwargs):
+    '''
+    method to run the module and/or its input filter
+    only the module input filter is run if kwargs is: test_filter=True
+    method is defined outside the class so that it can be used 
+    by other programs such as test_module and test_module_filter
+    '''
+
+    svariables = {}
+
+    svariables['runname'] = (self.runname, 'string')
+    svariables['path'] = (self.inpath, 'string')
+    svariables['pdbmol1'] = (self.pdbmol1, 'string')
+    svariables['pdbmol2'] = (self.pdbmol2, 'string')
+    svariables['infile'] = (self.infile, 'string')
+    svariables['ofile'] = (self.ofile, 'string')
+    svariables['basis1'] = (self.basis1, 'string')
+    svariables['basis2'] = (self.basis2, 'string')
+    svariables['lowres1'] = (self.lowres1, 'int')
+    svariables['lowres2'] = (self.lowres2, 'int')
+    svariables['highres1'] = (self.highres1, 'int')
+    svariables['highres2'] = (self.highres2, 'int')
+    svariables['ebasis1'] = (self.ebasis1, 'string')
+    svariables['ebasis2'] = (self.ebasis2, 'string')
+
+    svariables['zflag'] = (self.zflag, 'boolean')
+    svariables['zcutoff'] = (self.zcutoff, 'float')
+
+    error, self.variables = input_filter.type_check_and_convert(svariables)
+
+    if(len(error) > 0):
+        print 'error = ', error
+        if not(self.testflag):
+            sys.exit()
+        return error
+
+    try:
+        if kwargs['file_check']:
+            error = align_filter.check_align(self.variables)
+    except:
+        error = align_filter.check_align(self.variables, no_file_check="true")
+
+    if(len(error) > 0):
+        print 'error = ', error
+        if not(self.testflag):
+            sys.exit()
+        return error
+
+    try:
+        if kwargs['test_filter']:
+            return error
+    except:
+        pass
+
+    runname = self.variables['runname'][0]
+
+    if os.path.exists(os.path.join(runname, self.module)):
+        shutil.rmtree(os.path.join(runname, self.module))
+
+    txtQueue = multiprocessing.JoinableQueue()
+    align.align(self.variables, txtQueue)
+
+
+class gui_mimic_align():
+    '''
+    gui_mimic class contains the name of the module
+    '''
+    module = 'align'
+
+    def __init__(self, test, paths):
+
+        if not test:
+            user_variables(self)
+        else:
+            test_variables(self, paths)
+
+        run_module(self)
+
+
+if __name__ == '__main__':
+
+    test = False  # option to run with test variables not implemented in 1.0.
+    paths = None
+
+
+# We are thinking of defining the install path so the gui mimic can be run from anywhere as long as it is called from that particular python
+# That way, the test files will always be available to the user.
+    if test:
+        pdb_data_path = os.path.join(os.path.dirname(os.path.realpath(
+            __file__)), '..', '..', 'data', 'pdb_common') + os.path.sep
+        dcd_data_path = os.path.join(os.path.dirname(os.path.realpath(
+            __file__)), '..', '..', 'data', 'dcd_common') + os.path.sep
+        module_data_path = os.path.join(os.path.dirname(os.path.realpath(
+            __file__)), '..', '..', 'data', 'interface', 'align') + os.path.sep
+
+        paths = {'pdb_data_path': pdb_data_path,
+                 'dcd_data_path': dcd_data_path, 'module_data_path': module_data_path}
+
+    start = time.time()
+    run_gui = gui_mimic_align(test, paths)
+    print "time used: ", time.time() - start
