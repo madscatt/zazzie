cb9a6d1908b0bf89b17a365c37784a400c9d16a0
Joseph E. Curtis <madscatt@gmail.com>
2017-08-18 11:15:18 -0400

    state save; checking run on multiple cores

--- src/sassie/build/pdbrx/multi_run/multicore_run.py
+++ src/sassie/build/pdbrx/multi_run/multicore_run.py
@@ -1,11 +1,13 @@
-import os, glob
-import multiprocessing 
+import os
+import glob
+import multiprocessing
 
 import traceback
 import time
 import sys
 sys.path.append('./')
 import gui_mimic_pdbrx
+
 
 class Process(multiprocessing.Process):
     def __init__(self, *args, **kwargs):
@@ -31,106 +33,121 @@ class Process(multiprocessing.Process):
 
 def run_pdbrx(start, end, all_pdb_files, completed_pdb_files_directory, failed_pdb_files_directory):
 
-	#time.sleep(10)
-	fail = False
-        test = False
+        # time.sleep(10)
+    fail = False
+    test = False
 
-	for job in xrange(end - start + 1):
-		this_file = all_pdb_files[job + start]
-		head, tail = os.path.split(this_file)	
+    for job in xrange(end - start + 1):
+        this_file = all_pdb_files[job + start]
+        head, tail = os.path.split(this_file)
 
-                this_runname = 'run_'+tail[:-4]
+        this_runname = 'run_' + tail[:-4]
 
-                try:
-                    run_gui = gui_mimic_pdbrx.gui_mimic_pdbrx(test, \
-                                          runname=this_runname, \
-                                          pdbfile=this_file, \
-                                          use_defaults=True )
-                    os.system('mv ' + this_runname + ' ' +
-                              completed_pdb_files_directory)
-                except:
-                    os.system('mv ' + this_runname + ' ' +
-                              failed_pdb_files_directory)
-                              
+        try:
+            run_gui = gui_mimic_pdbrx.gui_mimic_pdbrx(test,
+                                                      runname=this_runname,
+                                                      pdbfile=this_file,
+                                                      use_defaults=True)
+            os.system('mv -f ' + this_runname + ' ' +
+                      completed_pdb_files_directory)
+            os.system('mv -f ' + this_file + ' ' +
+                      completed_pdb_files_directory)
+        except:
+            os.system('mv -f ' + this_runname + ' ' +
+                      failed_pdb_files_directory)
 
-		#print 'cp ' + this_file + ' ' + completed_pdb_files_directory + os.sep + tail + '_' + str(start)
-		#os.system('cp ' + this_file + ' ' + completed_pdb_files_directory + os.sep + tail + '_' + str(start))
+        # print 'cp ' + this_file + ' ' + completed_pdb_files_directory + os.sep + tail + '_' + str(start)
+        #os.system('cp ' + this_file + ' ' + completed_pdb_files_directory + os.sep + tail + '_' + str(start))
 
 #		if job == 1:
-#			fail = True	
-		if fail:
-			raise ValueError('Something went wrong...')
+#			fail = True
+        if fail:
+            raise ValueError('Something went wrong...')
 
-	return 
+    return
 
 
 def main(all_pdb_files_directory, completed_pdb_files_directory, failed_pdb_files_directory, number_of_cores):
-	
-	all_pdb_files = glob.glob(all_pdb_files_directory + os.sep + '*.pdb')
 
-	number_of_files = len(all_pdb_files)
+    all_pdb_files = glob.glob(all_pdb_files_directory + os.sep + '*.pdb')
 
-	print 'number of pdb files found = ', number_of_files
+    number_of_files = len(all_pdb_files)
 
-	if(not os.path.exists(completed_pdb_files_directory)):
-		os.system('mkdir ' + completed_pdb_files_directory)
+    print 'number of pdb files found = ', number_of_files
 
-	if(not os.path.exists(failed_pdb_files_directory)):
-		os.system('mkdir ' + failed_pdb_files_directory)
+    if(not os.path.exists(completed_pdb_files_directory)):
+        os.system('mkdir ' + completed_pdb_files_directory)
 
-	start = [] ; end = []
+    if(not os.path.exists(failed_pdb_files_directory)):
+        os.system('mkdir ' + failed_pdb_files_directory)
 
-	remainder = number_of_files % number_of_cores
-	divisor = number_of_files / number_of_cores
-	print 'divisor = ', divisor
-	print 'remainder = ', remainder
+    start = []
+    end = []
 
-	'''
+    remainder = number_of_files % number_of_cores
+    divisor = number_of_files / number_of_cores
+    print 'divisor = ', divisor
+    print 'remainder = ', remainder
+
+    '''
 number of pdb files found =  7
 divisor =  2
 remainder =  1
 start =  [0, 3, 6]
 end =  [2, 5, 6]
 	'''
+    '''
+number of pdb files found =  2
+divisor =  2
+remainder =  0
+start =  [0]
+end =  [-1]
+        '''
 
-        if divisor == 1:
-            start.append(0)
-            end.append(0)
-        else:
-	    for i in xrange(number_of_cores):
-		    if i < number_of_cores - 1:
-	                this_start = i * (divisor + 1)
-	                start.append(this_start)
-		        end.append(this_start + divisor)
-	    else:
-	        this_start = i * (divisor + 1)
-	        start.append(this_start)
-	        end.append(this_start + remainder - 1)
+    if number_of_files == 1:
+        start.append(0)
+        end.append(0)
+    elif number_of_files == 2:
+        start.append(0)
+        end.append(1)
+    else:
+        for i in xrange(number_of_cores):
+            if i < number_of_cores - 1:
+                this_start = i * (divisor + 1)
+                start.append(this_start)
+                end.append(this_start + divisor)
+            else:
+                this_start = i * (divisor + 1)
+                start.append(this_start)
+                end.append(this_start + remainder - 1)
 
-	print 'start = ', start
-	print 'end = ', end
+    print 'start = ', start
+    print 'end = ', end
 
-	all_jobs = []
-        q = multiprocessing.Queue()
+    all_jobs = []
+    q = multiprocessing.Queue()
 
-	for i in xrange(number_of_cores):
-  
-     		try: 
-			this_process = multiprocessing.Process(target=run_pdbrx, args=(start[i], end[i], all_pdb_files, completed_pdb_files_directory, failed_pdb_files_directory))
-			this_process.start()
-		except Exception as error:
-			print 'error = ', error
-   
-		all_jobs.append(this_process)
+    for i in xrange(number_of_cores):
 
-	return
+        try:
+            this_process = multiprocessing.Process(target=run_pdbrx, args=(
+                start[i], end[i], all_pdb_files, completed_pdb_files_directory, failed_pdb_files_directory))
+            this_process.start()
+        except Exception as error:
+            print 'error = ', error
+
+        all_jobs.append(this_process)
+
+    return
+
 
 if __name__ == '__main__':
 
-	number_of_cores = 1
+    number_of_cores = 3
 
-	all_pdb_files_directory = 'nr_pdb_files'
-	completed_pdb_files_directory = 'completed_pdb_files'
-	failed_pdb_files_directory = 'failed_pdb_files'
+    all_pdb_files_directory = 'nr_pdb_files'
+    completed_pdb_files_directory = 'completed_pdb_files'
+    failed_pdb_files_directory = 'failed_pdb_files'
 
-	main(all_pdb_files_directory, completed_pdb_files_directory, failed_pdb_files_directory, number_of_cores)
+    main(all_pdb_files_directory, completed_pdb_files_directory,
+         failed_pdb_files_directory, number_of_cores)
