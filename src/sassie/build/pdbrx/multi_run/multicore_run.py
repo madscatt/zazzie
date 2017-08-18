103192b8cb942c2962bedebfb6974373ecf4644e
Joseph E. Curtis <madscatt@gmail.com>
2017-08-18 10:39:24 -0400

    moved tmp_struct to run directory; moved *_log *_json files to run directory

--- src/sassie/build/pdbrx/multi_run/multicore_run.py
+++ src/sassie/build/pdbrx/multi_run/multicore_run.py
@@ -3,6 +3,9 @@ import multiprocessing
 
 import traceback
 import time
+import sys
+sys.path.append('./')
+import gui_mimic_pdbrx
 
 class Process(multiprocessing.Process):
     def __init__(self, *args, **kwargs):
@@ -30,12 +33,28 @@ def run_pdbrx(start, end, all_pdb_files, completed_pdb_files_directory, failed_p
 
 	#time.sleep(10)
 	fail = False
+        test = False
 
 	for job in xrange(end - start + 1):
 		this_file = all_pdb_files[job + start]
 		head, tail = os.path.split(this_file)	
-		print 'cp ' + this_file + ' ' + completed_pdb_files_directory + os.sep + tail + '_' + str(start)
-		os.system('cp ' + this_file + ' ' + completed_pdb_files_directory + os.sep + tail + '_' + str(start))
+
+                this_runname = 'run_'+tail[:-4]
+
+                try:
+                    run_gui = gui_mimic_pdbrx.gui_mimic_pdbrx(test, \
+                                          runname=this_runname, \
+                                          pdbfile=this_file, \
+                                          use_defaults=True )
+                    os.system('mv ' + this_runname + ' ' +
+                              completed_pdb_files_directory)
+                except:
+                    os.system('mv ' + this_runname + ' ' +
+                              failed_pdb_files_directory)
+                              
+
+		#print 'cp ' + this_file + ' ' + completed_pdb_files_directory + os.sep + tail + '_' + str(start)
+		#os.system('cp ' + this_file + ' ' + completed_pdb_files_directory + os.sep + tail + '_' + str(start))
 
 #		if job == 1:
 #			fail = True	
@@ -74,15 +93,19 @@ start =  [0, 3, 6]
 end =  [2, 5, 6]
 	'''
 
-	for i in xrange(number_of_cores):
-		if i < number_of_cores - 1:
-			this_start = i * (divisor + 1)
-			start.append(this_start)
-			end.append(this_start + divisor)
-		else:
-			this_start = i * (divisor + 1)
-			start.append(this_start)
-			end.append(this_start + remainder - 1)
+        if divisor == 1:
+            start.append(0)
+            end.append(0)
+        else:
+	    for i in xrange(number_of_cores):
+		    if i < number_of_cores - 1:
+	                this_start = i * (divisor + 1)
+	                start.append(this_start)
+		        end.append(this_start + divisor)
+	    else:
+	        this_start = i * (divisor + 1)
+	        start.append(this_start)
+	        end.append(this_start + remainder - 1)
 
 	print 'start = ', start
 	print 'end = ', end
@@ -104,9 +127,9 @@ end =  [2, 5, 6]
 
 if __name__ == '__main__':
 
-	number_of_cores = 3
+	number_of_cores = 1
 
-	all_pdb_files_directory = 'pdb_files'
+	all_pdb_files_directory = 'nr_pdb_files'
 	completed_pdb_files_directory = 'completed_pdb_files'
 	failed_pdb_files_directory = 'failed_pdb_files'
 
