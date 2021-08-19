a372e398967741bcc01ca23681259989dab887a2
Susan Krueger <skrueger@Susans-MBP.fios-router.home>
2021-08-19 16:48:20 -0400

    first running version of multi_component_analysis with gui mimic

--- susan_in_progress/gui_mimic_multi_component_analysis_21aug21.py
+++ susan_in_progress/gui_mimic_multi_component_analysis_21aug21.py
@@ -13,9 +13,8 @@ import os
 import shutil
 import time
 
-#import sassie.contrast.multicomponent_analysis.stoichiometry as stoichiometry
-import stoichiometry as stoichiometry
-#only stoichiometry is imported for now; others will be added later
+#import sassie.contrast.multi_component_analysis.multi_component_analysis as multi_component_analysis
+import multi_component_analysis_19aug21 as multi_component_analysis
 import sassie.interface.input_filter_sasmol as input_filter
 #import sassie.interface.multi_component_analysis.multi_component_analysis_filter as multi_component_analysis_filter
 #no module input filer for now
@@ -27,21 +26,19 @@ def user_variables(self, **kwargs):
     #### user input ####
     #### user input ####
 
-    self.run_name = 'run_0'
+#NOTE: 'runname' is hardwired in run.utils.general_setup, so it can't be changed to 'run_name' here!!
+    self.runname = 'run_0'
     self.path = './'
     self.number_of_contrast_points = '3' # must be >= number of components (2 in this case)
     self.number_of_components = '2'
     self.read_from_file = '0' #0=don't read from contrast calculator output file, 1=read from contrast calculator output file    
-    self.output_file_name = os.path.join(path,'99_12_41.out')
-#    self.input_file_name = os.path.join(path,'input_contrast.txt')
-    self.input_file_name = ''
+    self.output_file_name = os.path.join(self.path,'99_12_41.out')
+    self.input_file_name = os.path.join(self.path,'input_contrast.txt') #this only matters if read_from_file = 1
     self.fraction_d2o = [u'0.99', u'0.12', u'0.41']  # 1 value for each contrast
     self.partial_specific_volume = [u'0.745', u'0.903']  # 1 value for each component
     self.izero = [u'11.8', u'0.6', u'0.17']  # 1 value for each contrast
     self.concentration = [u'3.7', u'3.6', u'3.1']  # 1 value for each contrast
-    self.delta_rho = [[u'-3.2', u'-5.7'], [u'1.6', u'0.26'], [u'0.031', u'-1.74']]  # 2 values for each contrast since there are 2 components
-#do we need a blank delta_rho for the case where it is read from file? This should be taken care of in the initialization of the array in read_contrast_output_files?  Will need to test.
-
+    self.delta_rho = [[u'-3.2', u'-5.7'], [u'1.6', u'0.26'], [u'0.031', u'-1.74']]  # 2 values for each contrast since there are 2 components.  This will be superceded by the new values read from an input file if read_from_file = 1.
     self.match_point_flag = False
 
     self.stuhrman_parallel_axis_flag = False
@@ -72,7 +69,7 @@ def test_variables(self,paths):
     module_data_path = paths['module_data_path']
     other_data_path = paths['other_data_path']
 
-    self.run_name = 'run_0'
+    self.runname = 'run_0'
     self.path = ''
     self.number_of_contrast_points = '3'
     self.number_of_components = '2'
@@ -88,6 +85,7 @@ def test_variables(self,paths):
     self.stuhrman_parallel_axis_flag = False
     self.decomposition_flag = False
     self.stoichiometry_flag = True
+    self.stoichiometry_variables = [self.fraction_d2o,self.partial_specific_volume,self.izero,self.concentration,self.delta_rho]  
 
     self.precision = 3 #not needed? What is the default?
     self.testflag = True
@@ -102,8 +100,8 @@ def run_module(self, **kwargs):
 
     svariables={}
 
-    svariables['run_name'] = (self.run_name,'string')
-    svariables['path'] = (self.run_name,'string')
+    svariables['runname'] = (self.runname,'string')
+    svariables['path'] = (self.path,'string')
     svariables['output_file_name'] = (self.output_file_name,'string')
     svariables['input_file_name'] = (self.input_file_name,'string')
     svariables['stoichiometry_flag'] = (self.stoichiometry_flag,'boolean')
@@ -117,22 +115,23 @@ def run_module(self, **kwargs):
 
     error, self.variables = input_filter.type_check_and_convert(svariables)
     if len(error) > 0:
-        print 'error = ', error
+        print('error = ', error)
         if not(self.testflag):
             sys.exit()
         return error
 
-    try:
-        if kwargs['file_check']:
-            error = multi_component_analysis_filter.check_multi_component_analysis(self.variables, self.fraction_d2o, self.partial_specific_volume, self.izero, self.concentration, self.delta_rho)
-    except:
-            error = multi_component_analysis_filter.check_multi_component_analysis(self.variables, self.fraction_d2o, self.partial_specific_volume, self.izero, self.concentration, self.delta_rho, no_file_check="true")
+#only stoichiometry variables are checked for now; others to be added later
+#    try:
+#        if kwargs['file_check']:
+#            error = multi_component_analysis_filter.check_multi_component_analysis(self.variables, self.stoichiometry_variables)
+#    except:
+#            error = multi_component_analysis_filter.check_multi_component_analysis(self.variables, #self.stoichiometry_variables, no_file_check="true")
 
-    if(len(error) > 0):
-        print 'error = ', error
-        if not(self.testflag):
-            sys.exit()
-        return error
+#    if(len(error) > 0):
+#        print('error = ', error)
+#        if not(self.testflag):
+#            sys.exit()
+#        return error
 
     try:
         if kwargs['test_filter']:
@@ -140,17 +139,18 @@ def run_module(self, **kwargs):
     except:
         pass
 
-    run_name = self.variables['run_name'][0]
+    runname = self.variables['runname'][0]
 
-    if os.path.exists(os.path.join(run_name, self.module)):
-        shutil.rmtree(os.path.join(run_name, self.module))
+#I don't want the directory tree to be deleted since I want to be able to put more than one output file in the directory for scenarios involving different contrasts.  But, then the sassie_json and sassie_log files don't get removed even if an output file is overwritten.
+#    if os.path.exists(os.path.join(runname, self.module)):
+#        shutil.rmtree(os.path.join(runname, self.module))
 
     txtQueue = multiprocessing.JoinableQueue()
     this_multi_component_analysis = multi_component_analysis.multi_component_analysis()
-    this_multi_component_analysis.main(self.variables, txtQueue)
+    this_multi_component_analysis.main(self.variables, self.stoichiometry_variables, txtQueue)
 
 
-class gui_mimic_data_interpolation():
+class gui_mimic_multi_component_analysis():
     '''
     gui_mimic class contains the name of the module
     '''
@@ -187,6 +187,6 @@ if __name__ == '__main__':
 
     start = time.time()
     run_gui = gui_mimic_multi_component_analysis(test, paths)
-    print "time used: ", time.time() - start
+    print('time used: ', time.time() - start)
 
 
