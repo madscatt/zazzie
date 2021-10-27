9809ef6ad5f9eecab26230ffb9d6b005c0653e1f
Susan Krueger <skrueger@Susans-MBP.fios-router.home>
2021-10-27 10:19:03 -0400

    multi component analysis with stoichoimetry and matchpoint methods

---
+++ src/sassie/contrast/multi_component_analysis/multi_component_analysis_filter.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+
+#    SASSIE: Copyright (C) 2011 Joseph E. Curtis, Ph.D. 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#       MULTI-COMPONENT ANALYSIS FILTER
+#
+#       08/30/2021       --      initial coding         :   Susan Krueger
+#
+# LC      1         2         3         4         5         6         7
+# LC4567890123456789012345678901234567890123456789012345678901234567890123456789
+#                                                                      *      **
+'''
+    MULTI-COMPONENT ANALYSIS FILTER is the method that checks the inputs for
+    the MULTI-COMPONENT ANALYSIS module that were not previously checked by
+    INPUT FILTER, which only checks for valid string, float, integer, boolean, etc.
+        
+    Called from GUI_MIMIC_MULTI_COMPONENT_ANALYSIS
+    Calls INPUT_FILTER
+          
+    INPUTS:
+        run_name
+        output_file_name
+        input_file_name
+        read_from_file
+        number_of_contrast_points
+        number_of_components
+        stoichiometry_flag
+        match_point_flag
+        stuhrmann_parallel_axis_flag
+        decomposition_flag
+        fraction_d2o
+        initial_matchpoint_guess
+        izero
+        izero_error
+        concentration
+        concentration_error
+        partial_specific_volume
+        delta_rho
+            
+    OUTPUTS:
+        error string 
+'''
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+import sys
+import locale
+import string
+import sassie.interface.input_filter_sasmol as input_filter
+
+
+#this version uses the new input filter that recognizes a nested_float_array
+
+
+def check_multi_component_analysis(variables, **kwargs):
+
+    run_name = variables['run_name'][0]
+    output_file_name = variables['output_file_name'][0]
+    input_file_name = variables['input_file_name'][0]
+    read_from_file = variables['read_from_file'][0]
+    number_of_contrast_points = variables['number_of_contrast_points'][0]
+    fraction_d2o = variables['fraction_d2o'][0]
+    stoichiometry_flag = variables['stoichiometry_flag'][0]
+    match_point_flag = variables['match_point_flag'][0] 
+    stuhrmann_parallel_axis_flag = variables['stuhrmann_parallel_axis_flag'][0]
+    decomposition_flag = variables['decomposition_flag'][0]
+
+#check run_name
+    error = []
+    error = input_filter.check_name(run_name)
+    if(error != []):
+        return error
+
+#check read_from_file (Will an error be raised prior to this point?  YES! The program will crash in the input filter, so this test isn't needed. 
+    if(read_from_file != True and read_from_file != False):
+        error.append('read_from_file must be True or False')
+        return error
+
+#check that input file exists
+    if(read_from_file == True):
+        error = input_filter.check_file_exists(input_file_name)
+        if(len(error) > 0):
+            error.append('input file is not readable or does not exist')
+            return error
+
+#check that at least one method is chosen.
+    if(not stoichiometry_flag and not match_point_flag and not stuhrmann_parallel_axis_flag and not decomposition_flag):
+        error.append('at least one method must be selected')
+        return error
+
+#TODO:  Do we need to check of only one flag is True? Will more than one flag be allowed to be True at a time to execute methods sequentially? Right now, this is not the way the program is envisioned.  Like MulCh, you would choose a method and execute it.  Then choose another an execute, etc.  If the method is selected by a drop down menu, then we don't need to check to make sure that at least one flag is chosen and that only one flag is True.
+        
+#check the multi_component analysis variables depending on the method.
+    print('len frac d2o: ', len(fraction_d2o))
+#variables common to all methods:  fraction_d2o
+#check if length of fraction D2O = number of contrasts
+    if(len(fraction_d2o) != number_of_contrast_points):
+        error.append('fraction D2O must have %i values' %(number_of_contrast_points))
+        return error
+#check that fraction D2O is between 0 and 1
+    for i in range(number_of_contrast_points):
+        if(fraction_d2o[i] < 0 or fraction_d2o[i] > 1):
+            error.append('fraction D2O[%i] must be between 0 and 1' %(i))
+            return error
+
+#TODO: make only one test for variables common to more than one method? Would need to test for one method OR another and put a single test there.
+
+
+    if(stoichiometry_flag == True):
+        number_of_components = variables['number_of_components'][0]
+        izero = variables['izero'][0]
+        concentration = variables['concentration'][0]
+        partial_specific_volume = variables['partial_specific_volume'][0]
+        delta_rho = variables['delta_rho'][0]
+
+#check if number of contrast points is >= the number of components
+        if(number_of_contrast_points < number_of_components):
+            error.append('number of contrasts must be >= number of components')
+            return error
+#check if length of izero, concentration and delta_rho = number of contrasts
+        if(len(izero) != number_of_contrast_points):
+            error.append('I(0) must have %i values' %(number_of_contrast_points))
+            return error
+        if(len(concentration) != number_of_contrast_points):
+            error.append('concentration must have %i values' %(number_of_contrast_points))
+            return error
+#If read_from_file is True, the values are read from a contrast calculator output file, which is done in the unpack variables method in the main program. The way the program is written now, this executes after these checks are performed.  If we execute the reading of the values from the file to fill the value into the GUI before executing the main program, then we can perform this whether read_from_file is True or False and the if statement can be removed.
+        if(read_from_file == False):
+            if(len(delta_rho) != number_of_contrast_points):
+                error.append('delta rho must have %i sets of values' %(number_of_contrast_points))
+                return error
+#check if length delta_rho[i] = number of components. 
+            for i in range(number_of_contrast_points):
+                if(len(delta_rho[i]) != number_of_components):
+                    error.append('delta rho[%i] must have %i values' %(i,number_of_components))
+                    return error
+#check if length of partial specific volume = number of components
+        if(len(partial_specific_volume) != number_of_components):
+            error.append('partial_specific_volume must have %i values' %(number_of_components))
+            return error
+
+
+    elif(match_point_flag == True):
+        izero = variables['izero'][0]
+        izero_error = variables['izero_error'][0]
+        concentration = variables['concentration'][0]
+        concentration_error = variables['concentration_error'][0]
+        initial_match_point_guess = variables['initial_match_point_guess'][0]
+        print('init matchpoint guess: ', initial_match_point_guess)
+        
+#check if length of izero, izero_error, concentration and concentration_error = number of contrasts
+        if(len(izero) != number_of_contrast_points):
+            error.append('I(0) must have %i values' %(number_of_contrast_points))
+            return error
+        if(len(izero_error) != number_of_contrast_points):
+            error.append('I(0) error must have %i values' %(number_of_contrast_points))
+            return error
+        if(len(concentration) != number_of_contrast_points):
+            error.append('concentration must have %i values' %(number_of_contrast_points))
+            return error
+        if(len(concentration_error) != number_of_contrast_points):
+            error.append('concentration error must have %i values' %(number_of_contrast_points))
+            return error
+#check if izero_error and concentration_error are non-zero since a weighted fit is performed
+        for i in range(number_of_contrast_points):
+                if(izero_error[i] == 0.0):
+                    error.append('I(0) error[%i] cannot equal zero' %(i))
+                    return error
+                if(concentration_error[i] == 0.0):
+                    error.append('concentration error[%i] cannot equal zero' %(i))
+                    return error
+#check that initial match point guess is between 0 and 1
+        if(initial_match_point_guess < 0 or initial_match_point_guess > 1):
+            error.append('initial match point guess must be between 0 and 1')
+            return error
+
+    return error
+
+
---
+++ src/sassie/contrast/multi_component_analysis/stoichiometry.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+
+#    SASSIE: Copyright (C) 2011 Joseph E. Curtis, Ph.D. 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#       MULTI COMPONENT ANALYSIS: STOICHIOMETRY
+#
+#       10/22/2021       --      initial coding         :   Susan Krueger
+#
+# LC      1         2         3         4         5         6         7
+# LC4567890123456789012345678901234567890123456789012345678901234567890123456789
+#                                                                      *      **
+'''
+
+    STOICHIOMETRY contains the GET MOLECULAR WEIGHTS method and related functions that calculate the molecular weights of the components in a complex containing multiple copies of two or more components..
+    
+    INPUTS: 
+        I(0), concentration, fraction D2O for each contrast
+        contrast values (delta_rho) for each component at each contrast
+    
+    OUTPUTS:
+        optimized molecular weight for each component
+    
+    Called by the MULTI-COMPONENT ANALYSIS module.
+    
+'''
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import io
+import os
+import sys
+import string
+import locale
+import time
+import numpy
+import scipy.optimize
+
+
+def get_molecular_weights(other_self):
+    '''
+    GET MOLECULAR WEIGHTS is the Stoichiometry Analysis method that calculates the molecular weights of the components in a complex containing multiple copies of two or more components.
+     Executed if stoichiometry_flag == True.
+    
+    This method calls MOLECULAR WEIGHT FUNCTION.
+    
+    INPUTS:
+
+        module variables
+            run_name:                           name of directory containing the outputs
+            input_file_name:                    name of file containing contrast values calculated from the contrast calculator module
+            read_from_file:                     True if contrast values are being read from an input file with chosen input_file_name; False if contrast values are being input by the user
+            number_of_contrast_points:          number of contrasts being used in the molecular weights calculation
+            number_of_components:               number of components with different contrasts in the complex
+            fraction_d2o array:                 the fraction of D2O in the solvent for each contrast
+            izero array:                        I(0) value at each contrast
+            concentration array:                concentration at each contrast
+            partial_specific_volume array:      partial specific volume of each component
+            delta_rho matrix:                   contrast value for each component at each fraction of D2O
+
+        multi_component_analysis_variables
+            output_file_name:                   name of output file
+        
+    OUTPUTS:
+        files stored in output file directory (need boilerplate language to add variable name/path for the directory):
+
+            output file with chosen output_file_name containing:
+                module variable input parameters
+                calculated molecular weights of the components in kDa
+                total molecular weight of the complex in kDa
+                weight fractions of each component
+
+    '''
+#mvars used:  fraction_d2o, number_of_contrast_points, partial_specific_volume, izero, concentration, delta_rho 
+#mcavars used:  outfile
+
+
+    
+    log = other_self.log
+    log.debug('in get_molecular_weights')
+    pgui = other_self.run_utils.print_gui
+
+    mvars = other_self.module_variables
+    mcavars = other_self.multi_component_analysis_variables
+    
+    log.debug(vars(mvars))
+    log.debug(vars(mcavars))
+    
+
+    ttxt = time.asctime(time.gmtime(time.time()))
+    st = ''.join(['=' for x in xrange(60)])
+
+    pgui('\n%s \n' % (st))
+    pgui('DATA FROM RUN: %s \n\n' % (ttxt))
+
+    Na = 6.023  #10**23
+
+    partial_specific_volume_1 = mvars.partial_specific_volume[0]
+    partial_specific_volume_2 = mvars.partial_specific_volume[1]
+#   print ('partial_specific_volume_1,partial_specific_volume_2: ', partial_specific_volume_1, partial_specific_volume_2)
+
+#   print ("number of contrast points: ", number_of_contrast_points)
+
+    pgui('Stoichiometry method\n')
+    pgui('setting up I(0) equation coefficients')
+        
+    if mvars.read_from_file == True:
+        #pgui('\n')
+        pgui('contrast values read from input file: %s' % (mvars.input_file_name))
+        
+    x = numpy.zeros(shape=(mvars.number_of_contrast_points, 4))
+
+    # Here we are defining the x coeficients at each contrast for the set of simultaneous I(0) equations.
+    # NOTE that range is being used here since xrange in Python 2 == range in Python 3. So, we aren't doing exactly the same thing here as will be done in Python 3, as range will behave like Python 2's xrange in Python 3.  But, for this case, it shouldn't matter (if I understand correctly).
+    for i in range(mvars.number_of_contrast_points):
+        delta_rho_1 = mvars.delta_rho[i][0]
+        delta_rho_2 = mvars.delta_rho[i][1]
+        izero_1 = mvars.izero[i]
+        concentration_1 = mvars.concentration[i]
+    # print('delta_rho_1, delta_rho_2, izero, concentration: ', delta_rho_1, delta_rho_2, izero_1, concentration_1)
+        x0 = delta_rho_1**2*partial_specific_volume_1**2
+        x1 = 2*delta_rho_1*delta_rho_2*partial_specific_volume_1*partial_specific_volume_2
+        x2 = delta_rho_2**2*partial_specific_volume_2**2
+        x3 = izero_1*Na/concentration_1
+        x[i] = (x0, x1, x2, x3)
+
+    # print ('x: ', x)
+    # print ('length of x: ', len(x))
+
+    y = numpy.zeros(len(x))
+    # print ('y: ', y)
+
+    # transpose x to have correct inputs for curve_fit, which accepts a 1 column matrix with number of rows = number of contrast points.  We have a 1 row matrix of length = number of contrast points.
+    x = x.T  
+
+    pgui('calculating molecular weights')
+    
+
+    '''
+    scipy.optimize.curve_fit is being used to solve the set of simultaneous equations
+    using non-linear least squares fitting.  It uses scipy version 1.2.1 in Python 2.7.
+    There is no known change of syntax or usage if using scipy 1.7 under Python 3.
+    
+    optimized_molecular_weights are the optimal values for the molecular weights such that
+    the sum of the squared error of izero_function(x, optimized_molecular_weights) - y
+    is minimized. 
+        
+    The y array = 0 since izero_function is defined to be equal to zero
+    The x array = the values of the coefficients that consist of the known parameters in izero_function
+    once it is written in terms of the individual M_{w} values.
+    
+    '''
+
+    optimized_molecular_weights, covariance = scipy.optimize.curve_fit(molecular_weight_function, x, y)
+
+#TODO: decide what results should be in the output file and what should be printed out to the GUI screen.  Also, Test some errors on the coefficients (hardwired) here to see if we should be allowing users to enter errors. Look at Watson et al power law paper for ranges of values for molecular volume. Would we ask for errors on each parameter, i.e., izero, delta_rho, concentration, etc. and then propagate them here when we calculate the coefficients? If we decide to include errors, then we can report the covariance matrix and standard deviations. 
+
+#    pgui('calculated molecular_weight_1,molecular_weight_2: '+str(optimized_molecular_weights)+'\n')
+#    mcavars.outfile.write('calculated molecular_weight_1,molecular_weight_2: '+str(optimized_molecular_weights)+'\n')
+#    pgui('calculated covariance: '+str(covariance)+'\n')
+#    mcavars.outfile.write('calculated covariance: '+str(covariance)+'\n')
+
+    pgui('results written to output file: %s' % (mcavars.multi_component_analysis_path+mvars.output_file_name))
+    pgui('-------------------------------')
+    mcavars.outfile.write('--------------------------------\n')
+    pgui('Final Results\n')
+    mcavars.outfile.write('Final Results\n')
+
+    # convert molecular weights to kDa
+    molecular_weight_1 = optimized_molecular_weights[0]*10**3
+    molecular_weight_2 = optimized_molecular_weights[1]*10**3
+    pgui('molecular_weight_1, molecular_weight_2 (kDa): '+str(molecular_weight_1)+'\t'+str(molecular_weight_2)+'\n')
+    mcavars.outfile.write('molecular_weight_1, molecular_weight_2 (kDa): '+str(molecular_weight_1)+'\t'+str(molecular_weight_2)+'\n')
+
+
+    # The covariance matrix will be infinite if there are only two contrasts, so we don't want to calculate standard deviations in this case; numpy.all tests whether all array elements along a given axis evaluate to True.
+#    if numpy.all(covariance != numpy.inf):
+
+        # rescale values in covariance matrix since molecular weights were converted to kDa
+#        rescaled_covariance = covariance*10**6
+#        pgui('covariance matrix: ' + str(rescaled_covariance)+'\n')
+#        mcavars.outfile.write('covariance matrix: ' + str(rescaled_covariance)+'\n')
+#        # compute one standard deviation of the molecular weights
+#        standard_deviation_1 = numpy.sqrt(rescaled_covariance[0][0])
+#        standard_deviation_2 = numpy.sqrt(rescaled_covariance[1][1])
+#        pgui('standard_deviation_1, standard_deviation_2 (kDa): ' +
+#                  str(standard_deviation_1)+'\t'+str(standard_deviation_2)+'\n')
+#        mcavars.outfile.write('standard_deviation_1, standard_deviation_2 (kDa): ' +
+#                  str(standard_deviation_1)+'\t'+str(standard_deviation_2)+'\n')
+#        correlation_coefficient = rescaled_covariance[0][1]/(standard_deviation_1*standard_deviation_2)
+#        pgui('correlation coefficient: '+str(correlation_coefficient)+'\n')
+#        mcavars.outfile.write('correlation coefficient: '+str(correlation_coefficient)+'\n')
+
+    total_molecular_weight = molecular_weight_1 + molecular_weight_2
+    weight_fraction_1 = molecular_weight_1/total_molecular_weight
+    weight_fraction_2 = molecular_weight_2/total_molecular_weight
+
+# Do we want to calculate volume fractions here as well? Will they be needed?
+
+    pgui('Total Mw (kDa), weight_fraction_1, weight_fraction_2: '+str(total_molecular_weight)+'\t'+str(weight_fraction_1)+'\t'+str(weight_fraction_2))
+    mcavars.outfile.write('Total Mw (kDa), weight_fraction_1, weight_fraction_2: '+str(total_molecular_weight)+'\t'+str(weight_fraction_1)+'\t'+str(weight_fraction_2))
+    mcavars.outfile.close()
+
+    return
+
+def molecular_weight_function(x, molecular_weight_1, molecular_weight_2):
+#TODO: generalize this function any number of components? Or offer hardwired options for 2 and 3 components?
+    r'''
+    
+    MOLECULAR WEIGHT FUNCTION is the equation 
+      
+    n(\sum_{i} \Delta  \rho_{i}V_{i})^{2} - I(0) = 0, where 
+     
+    I(0) is the intensity at q=0
+    \Delta \rho_{i} is the contrast of i^{th} component
+    V_{i} is the volume of i^{th} component
+    n = c\frac{N_{A}}{\sum_{i} (M_{w})_{i}} is the number density of particles where
+    c is the concentration
+    N_{A} is Avogadro's number and
+    \sum_{i} (M_{w})_{i} is the total molecular weight of the complex.
+    
+    If the volume is rewritten in terms of the partial specific volume, \overline{v},
+    
+    V = \overline{v} \frac {M_{w}}{N_{A}},
+    
+    and the sum over M_{w} is expanded, then the equation can be rewritten in terms
+     of the individual M_{w} values such that they can be obtained by solving a set
+    of simultaneous equations at each contrast.
+    
+    This function is called from GET MOLECULAR WEIGHTS.
+     
+    INPUTS:
+        module variables
+             x array:                array of x coefficients at each contrast (currently hardwired for 2 components)
+                x0:                 \Delta \rho_{1}^{2} \overline{v}_{1}^{2}
+                x1:                 2 \Delta \rho_{1} \Delta \rho_{2} \overline{v}_{1} \overline{v}_{2}
+                x2:                 \Delta \rho_{2}^{2} \overline{v}_{2}^{2}
+                x3:                 \frac(I(0) N_{A}/c)
+        
+    OUTPUTS: 
+        optimized molecular weights, (M_{w})_{1} and (M_{w})_{2}, for the components
+                        
+    '''
+    f = x[0] * molecular_weight_1**2 + x[1] * molecular_weight_1*molecular_weight_2 + x[2] * molecular_weight_2**2 - x[3] * molecular_weight_1 - x[3] * molecular_weight_2
+    return f
+
