e14e6cc68da621a92a8cbb0e39e26cc75b9ac498
Joseph Curtis <madscatt@gmail.com>
2024-07-25 11:22:15 -0400

    repo update

--- src/sassie/analyze/asaxs/asaxs_methods/scattering_label_and_sum.py
+++ src/sassie/analyze/asaxs/asaxs_methods/scattering_label_and_sum.py
@@ -8,3 +8,18 @@ import scipy as sp
 from scipy.constants import pi
 from scipy.special import sinc
 from scipy.interpolate import interp1d
+
+
+
+
+
+if __name__ == "__main__":
+
+    import sassie.analyze.asaxs.asaxs_methods.prototype_testing_data.scattering_label_and_sum_data.input_data as input_data
+
+    handles = input_data.initialize_input_data()
+
+    print('handles = ', handles)
+
+    pass
+
