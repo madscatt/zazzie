4bbb37c2d130a66bdd787f2e248a65848fe22ffc
Joseph Curtis <madscatt@gmail.com>
2024-07-26 15:57:46 -0400

    added calculate folder

---
+++ src/sassie/analyze/asaxs/asaxs_methods/construct_t_and_obtain_g.py
@@ -0,0 +1,40 @@
+
+import numpy
+import sasmol as system
+import math
+import sys
+import os
+import scipy
+from scipy.constants import pi
+from scipy.special import sinc
+from scipy.interpolate import interp1d
+
+import pprint
+
+
+def construct_t_and_obtain_g(handles):
+
+###  original python prototype by Benson Chan
+
+    pass
+
+    return
+
+
+
+if __name__ == "__main__":
+
+    import sassie.analyze.asaxs.asaxs_methods.prototype_testing_data.scattering_label_and_sum_data.input_data as input_data
+
+#    handles = input_data.initialize_input_data()
+
+    handles = {}
+
+    construct_t_and_obtain_g(handles)
+
+    print('done')
+
+    #print('handles = ', handles)
+
+    pass
+
