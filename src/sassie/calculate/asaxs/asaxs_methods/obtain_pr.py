e8209e39d9db89793c65a178426205a9e6279e92
Joseph Curtis <madscatt@gmail.com>
2024-07-26 17:16:47 -0400

    add pr method

---
+++ src/sassie/calculate/asaxs/asaxs_methods/obtain_pr.py
@@ -0,0 +1,40 @@
+
+import numpy
+import sasmol as system
+import math
+import sys
+import os
+import scipy
+from scipy.constants import pi
+from scipy.special import sinc
+from scipy.interpolate import interp1d
+
+import pprint
+
+
+def obtain_pr(handles):
+
+###  original python prototype by Benson Chan
+
+    pass
+
+    return
+
+
+
+if __name__ == "__main__":
+
+    #import sassie.calculate.asaxs.asaxs_methods.prototype_testing_data.scattering_label_and_sum_data.input_data as input_data
+
+#    handles = input_data.initialize_input_data()
+
+    handles = {}
+
+    obtain_pr(handles)
+
+    print('done')
+
+    #print('handles = ', handles)
+
+    pass
+
