facb8d9637e693657e7c945b0663ca9c55853d2e
Joseph Curtis <madscatt@gmail.com>
2024-07-28 13:02:33 -0400

    new script to write input data required by calcI_SlblAtoms() to a file to compare to Matlab results

--- src/sassie/calculate/asaxs/asaxs_methods/write_input_data_for_calcI_Slblatoms.py
+++ src/sassie/calculate/asaxs/asaxs_methods/write_input_data_for_calcI_Slblatoms.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pprint
 
 def write_input_data(handles, filename="input_data_for_calcI_Slblatoms.txt"):
@@ -10,7 +11,19 @@ def write_input_data(handles, filename="input_data_for_calcI_Slblatoms.txt"):
         file.write("Input data for calcI_SlblAtoms:\n")
         for key in required_keys:
             if key in handles:
-                file.write(f"{key}: {pprint.pformat(handles[key])}\n")
+                value = handles[key]
+                if isinstance(value, (list, np.ndarray)):
+                    file.write(f"{key}:\n")
+                    for item in value:
+                        if isinstance(item, (list, np.ndarray)):
+                            formatted_item = " ".join(
+                                str(int(x) + 1) if isinstance(x, int) else str(x) for x in item
+                            )
+                            file.write(formatted_item + "\n")
+                        else:
+                            file.write(str(int(item) + 1) if isinstance(item, int) else str(item) + "\n")
+                else:
+                    file.write(f"{key}: {pprint.pformat(value)}\n")
             else:
                 file.write(f"{key}: Key not found in handles\n")
 
@@ -18,14 +31,17 @@ def write_input_data(handles, filename="input_data_for_calcI_Slblatoms.txt"):
 
 print('DONE writing input data for calcI_SlblAtoms')
 
-# Write input data to file before calculating label-atom scattering I(S)
-#write_input_data_for_calcI_SlblAtoms(handles)
-
-# Calculate the label-atom scattering I(S)
-#handles, errorlg = calcI_SlblAtoms(handles)
-#if errorlg:
-#    raise Exception('Label-scatter intensity calc error')
-
-"""
-# Calculate the label-label scattering I(S)
-"""
+# Example usage
+# handles = {
+#     'nEnCalc': 10,
+#     'nDCalc': 5,
+#     'nSCalc': 3,
+#     'nlblTypes': 2,
+#     'nLabels': 7,
+#     'labelData': [[1, 2, 3], [4, 5, 6]],
+#     'atomData': np.array([[1, 2, 3], [4, 5, 6]]),
+#     'deltaD': 0.1,
+#     'fPairCalc': [0.2, 0.3],
+#     'actDMax': 1.0
+# }
+# write_input_data(handles)
